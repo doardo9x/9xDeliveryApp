9fc451b077f09898557b9ab628204e9f2e91012f
Doardo9x <eduardo010304@gmail.com>
2025-08-25 07:58:04 -0300

    Criando o projeto.

---
+++ doardoDelivery/constants/Colors.ts
@@ -0,0 +1,27 @@
+import { Background } from "@react-navigation/elements";
+
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#ffffff"
+
+export default{
+    light:{
+        text:'#0000000',
+        Background:'#ffffff',
+        tint: tintColorLight,
+        tabIconDefault: '#cccccc',
+        tabIconSelected: tintColorLight
+    },
+    dark:{
+        text:'#ffffff',
+        Background:'#000000',
+        tint: tintColorLight,
+        tabIconDefault: '#cccccc',
+        tabIconSelected: tintColorLight
+    },
+    primary:"#20E1B2",
+    lightGrey:"#FCF8FF",
+    grey:"#EEE9F0",
+    medium:"#9F9AA1",
+    mediumDark:"#424242",
+    green:"#437919"
+}
